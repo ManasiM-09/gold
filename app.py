533585e974893cf8af19ccc15280023a2370ea77
Ubuntu <ubuntu@ip-172-31-79-102.ec2.internal>
2025-12-23 13:31:12 +0000

    added new line

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 new file for git push command
 wornderful city i want to explore
+nothing to worry about anything 
+just go with flow
+you are my destiny 
+jay ho
+bohot mehenat ki hai
+yaha ane ke liye
+i want to fly soooo high
+evrything requires paitients 
+
